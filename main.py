c4b9bcb9b6f079ddee93a260e6fe2e6f2b7b946b
Ben McCarty <benmccarty7@gmail.com>
2018-03-27 14:19:47 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+﻿""" I'd like to make a platforming game in a metroidvania style including:
+    - discrete rooms
+    - non-linear map (which is to say, not just a sidescroller, a la the old Zelda games)
+    - jump mechanic
+    - focus on exploration rather than combat
+-> Probably going to be a proof of concept (which is to say, "the mechanics are done but not the other stuff")
+Main goals:
+    - Make a jumping mechanic
+    - Scroll between windows smoothly
+Main anti-goals:
+    - Keep the mechanics down. Designing too many mechanics makes a game cluttered and messy. Simple >>>
+    - mechanics => art every time it can look like crap so long as it works properly.
+Extension goals:
+    - Figure out a way to pick up items and have them change a mechanic (jump), changing the way the game is played.
+    - etc
+
+# Inspiration taken from http://store.steampowered.com/app/70300/VVVVVV/ which is an AMAZING game i highly recommend.
+"""
+"""
+edited game prompt:
+    - discrete rooms
+    - non-linear map
+    - jump mechanic
+    - exploration, not combat. each room should aim to be a puzzle
+Main goals:
+    - make a jumping mechanic
+    - figure out a way to jump off walls
+    
